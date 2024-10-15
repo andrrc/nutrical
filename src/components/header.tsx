98b83683e6c53a02da9456bc1ff08d7987a670ca
André Cassimiro <andrevieiracvf@gmail.com>
2024-10-15 19:14:42 -0300

    initial

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import "@/app/css/header.css";
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <h1>Nutrical</h1>
+                <ul>
+                    <li>IA</li>
+                    <li>Dietas</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}

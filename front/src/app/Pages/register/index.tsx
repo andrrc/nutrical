9b808f95f2c23ec06e4d38b6954529c511d5b331
jmoranj <morandim.joao@gmail.com>
2024-10-28 13:57:46 -0300

    Infraestructure

---
+++ front/src/app/Pages/register/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/header/header";
+import FormRegistro from "@/components/formRegister/formRegister";
+
+export default function Register() {
+  return (
+    <>
+      <Header />
+      <FormRegistro />
+    </>
+  );
+}
--- front/src/components/formRegister/formRegister.tsx
+++ front/src/components/formRegister/formRegister.tsx
@@ -1,14 +1,18 @@
-export default function formRegistro(){
-    return(
+export default function FormRegistro() {
+    return (
         <form method="GET" action="">
             <label>Nome:</label>
             <input type="text" />
+
             <label>Email:</label>
             <input type="text" />
+
             <label>Senha:</label>
             <input type="text" />
+
             <label>Peso:</label>
             <input type="text" />
+
             <label>Data de Nascimento:</label>
             <input type="date" />
         </form>
